694389b111ea36994dd3bd33efa3b89b61fee93b
boi-one <91535051+boi-one@users.noreply.github.com>
2024-10-14 15:22:31 +0200

    new branch

--- src/Grid.cpp
+++ src/Grid.cpp
@@ -2,28 +2,10 @@
 
 void Grid::Update(const Camera& camera)
 {
-	zoomedGridSpacing = gridSize * camera.zoom;
-
-	//position.x = fmod(camera.position.x, zoomedGridSpacing);
-	//position.y = fmod(camera.position.y, zoomedGridSpacing);
-
-	position.x = fmod(camera.position.x, gridSize) * zoomedGridSpacing;
-	position.y = fmod(camera.position.y, gridSize) * zoomedGridSpacing;
+	
 }
 
 void Grid::DrawGrid(ImDrawList* draw_list, const Camera& camera)
 {
-	for (float x = position.x; x < camera.size.x; x += zoomedGridSpacing)
-	{
-		ImVec2 startLine = ImVec2(x, 0);
-		ImVec2 endLine = ImVec2(x, camera.size.y);
-		draw_list->AddLine(startLine, endLine, ImColor(200, 0, 0, 255), .5f);
-	}
-	for (float y = position.y; y < camera.size.y; y += zoomedGridSpacing)
-	{
-		ImVec2 startLine = ImVec2(0, y);
-		ImVec2 endLine = ImVec2(camera.size.x, y);
-
-		draw_list->AddLine(startLine, endLine, ImColor(0, 200, 0, 255), .5f);
-	}
+	
 }
